9049e5794798c04d5feeaed292e7fb4d3225a48b
doxsch <28098153+doxsch@users.noreply.github.com>
2020-04-20 20:52:28 +0200

    added test script

--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-dirname
-basename
-pwd
+# customize with your own.
+options=("AAA" "BBB" "CCC" "DDD" "EEE")
 
-echo "test" >> ~/.cli_alias
+menu() {
+    echo "Avaliable options:"
+    for i in ${!options[@]}; do 
+        printf "%3d%s) %s\n" $((i+1)) "${choices[i]:- }" "${options[i]}"
+    done
+    if [[ "$msg" ]]; then echo "$msg"; fi
+}
+
+prompt="Check an option (again to uncheck, ENTER when done): "
+while menu && read -rp "$prompt" num && [[ "$num" ]]; do
+    [[ "$num" != *[![:digit:]]* ]] &&
+    (( num > 0 && num <= ${#options[@]} )) ||
+    { msg="Invalid option: $num"; continue; }
+    ((num--)); msg="${options[num]} was ${choices[num]:+un}checked"
+    [[ "${choices[num]}" ]] && choices[num]="" || choices[num]="+"
+done
+
+printf "You selected"; msg=" nothing"
+for i in ${!options[@]}; do 
+    [[ "${choices[i]}" ]] && { printf " %s" "${options[i]}"; msg=""; }
+done
+
+echo "$msg"
