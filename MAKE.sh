7f76d6f576f0f5f03a9a7ddc4ed527a7df04b5b9
doxsch <28098153+doxsch@users.noreply.github.com>
2020-04-22 22:19:39 +0200

    improved installer. added permanent install

--- MAKE.sh
+++ MAKE.sh
@@ -93,28 +93,4 @@ echo $OPTIONS
 echo "options=($OPTIONS)">./bash/install.sh
 echo "urls=($URLS)">>./bash/install.sh
 
-cat << 'END_OF_FILE' >> ./bash/install.sh
-menu() {
-    echo "Avaliable options:"
-    for i in ${!options[@]}; do 
-        printf "%3d%s) %s\n" $((i+1)) "${choices[i]:- }" "${options[i]}"
-    done
-    if [[ "$msg" ]]; then echo "$msg"; fi
-}
-
-prompt="Check an option (again to uncheck, ENTER when done): "
-while menu && read -rp "$prompt" num && [[ "$num" ]]; do
-    [[ "$num" != *[![:digit:]]* ]] &&
-    (( num > 0 && num <= ${#options[@]} )) ||
-    { msg="Invalid option: $num"; continue; }
-    ((num--)); msg="${options[num]} was ${choices[num]:+un}checked"
-    [[ "${choices[num]}" ]] && choices[num]="" || choices[num]="+"
-done
-
-printf "You selected"; msg=" nothing"
-for i in ${!options[@]}; do 
-    [[ "${choices[i]}" ]] && { printf " %s" "${urls[i]}"; msg=""; }
-done
-
-echo "$msg"
-END_OF_FILE
+cat $PROJECT_HOME/install.sh >> ./bash/install.sh
--- install.sh
+++ install.sh
@@ -1,61 +1,97 @@
 #!/bin/bash
 
 # customize with your own.
-options=("AAA" "BBB" "CCC" "DDD" "EEE")
+# options=( "cli/cd_aliases" "cli/ls_aliases" "npm/install_aliases" "npm/update_aliases")
+# urls=( https://raw.githubusercontent.com/doxsch/cli_aliases/master/dist/bash/cli/cd_aliases https://raw.githubusercontent.com/doxsch/cli_aliases/master/dist/bash/cli/ls_aliases https://raw.githubusercontent.com/doxsch/cli_aliases/master/dist/bash/npm/install_aliases https://raw.githubusercontent.com/doxsch/cli_aliases/master/dist/bash/npm/update_aliases)
+
 
 # define functions install / uninstall
 
+
 install() {
-    echo "$1" >> $saveFile
+    # $1 option $2 url
+    echo "source <(curl -s -H 'Cache-Control: no-cache' $2)" >> $profileFile 
+    # source <(curl -s -H 'Cache-Control: no-cache' $2)
+
+    echo ""
+    echo ""
+    echo ""
+    echo "------------------------------"
     echo "$1 installed"
+    echo "------------------------------"
+    echo ""
 }
 
 uninstall() {
-    cat ~/.cli_aliases | sed "s|^$1\$||g" | sed '/^$/d' > $saveFile
+    # $1 option $2 url
+    encodedUrl=$(echo "$2"|sed "s|\.|\\\.|g")
+    cat $profileFile | sed "s|^source <(curl -s -H 'Cache-Control: no-cache' $encodedUrl)\$||g" | sed '/^$/d' > $profileFile 
+
+    # unaliasUrl=$(echo $2 | sed "s|alias|unalias|g")
+    # echo $unaliasUrl
+    # source <(curl -s -H 'Cache-Control: no-cache' $unaliasUrl)
+
+    echo ""
+    echo ""
+    echo ""
+    echo "------------------------------"
     echo "$1 uninstalled"
+    echo "------------------------------"
+    echo ""
 }
+
+
+echo "       _  _             _  _                _____              _          _  _                       ___     ___     _ "
+echo "  ___ | |(_)      __ _ | |(_)  __ _  ___    \_   \ _ __   ___ | |_  __ _ | || |  ___  _ __  __   __ / _ \   / _ \   / |"
+echo " / __|| || |     / _\` || || | / _\` |/ __|    / /\/| '_ \ / __|| __|/ _\` || || | / _ \| '__| \ \ / /| | | | | | | |  | |"
+echo "| (__ | || |    | (_| || || || (_| |\__ \ /\/ /_  | | | |\__ \| |_| (_| || || ||  __/| |     \ V / | |_| |_| |_| |_ | |"
+echo " \___||_||_|_____\__,_||_||_| \__,_||___/ \____/  |_| |_||___/ \__|\__,_||_||_| \___||_|      \_/   \___/(_)\___/(_)|_|"
+echo "           |_____|                                                                                                     "
 
 
 #change to home directory
 cd ~
 
-saveFile=".cli_aliases"
+profileFile=".bash_profile"
 
 # create file if not exists
-test -f $saveFile || touch $saveFile
+test -f $profileFile || touch $profileFile
 
 # Read save file
 
 for i in ${!options[@]}; do
-    if grep -q "${options[i]}" "$saveFile"; then
+    if grep -q "${urls[i]}" "$profileFile"; then
         choices[i]="+"
     fi
 done
 
 menu() {
-    echo "Avaliable options:"
+    echo ""
+    echo "----------------------"
+    echo "| Avaliable aliases: |"
+    echo "----------------------"
     for i in ${!options[@]}; do 
         printf "%3d%s) %s\n" $((i+1)) "${choices[i]:- }" "${options[i]}"
     done
-    if [[ "$msg" ]]; then echo "$msg"; fi
+    # if [[ "$msg" ]]; then echo "$msg"; fi
 }
-
-prompt="Check an option (again to uncheck, ENTER when done): "
+echo "---------------------------------------------------------------"
+echo " # ) = not installed" 
+echo " #+) = already installed"
+echo "---------------------------------------------------------------"
+prompt="Select number to install this alias[es] (again to uninstall, Hit ENTER when done): "
 while menu && read -rp "$prompt" num && [[ "$num" ]]; do
     [[ "$num" != *[![:digit:]]* ]] &&
     (( num > 0 && num <= ${#options[@]} )) ||
     { msg="Invalid option: $num"; continue; }
     ((num--)); msg="${options[num]} was ${choices[num]:+un}checked"
     [[ "${choices[num]}" ]] && choices[num]="" || choices[num]="+"
-    [[ "${choices[num]}" ]] && install ${options[num]} || uninstall ${options[num]}
+    [[ "${choices[num]}" ]] && install ${options[num]} ${urls[num]} || uninstall ${options[num]} ${urls[num]}
 done
 
-printf "You selected"; msg=" nothing"
-for i in ${!options[@]}; do 
-    [[ "${choices[i]}" ]] && { printf " %s" "${options[i]}"; msg=""; }
-done
+source ~/.bash_profile
+printf "Exit Installer";
 
-echo "$msg"
 options=()
 choices=()
 
